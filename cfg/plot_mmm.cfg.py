d36ed5b7fb2e621243cd4aef6b04c08097e58a24
cesare borgia <v.stampf@icloud.com>
2019-11-14 17:18:04 +0100

    towards cfgs for everything

--- cfg/plot_mmm.cfg.py
+++ cfg/plot_mmm.cfg.py
@@ -1,8 +1,23 @@
 from plotter import Plotter
+from selections import Selections
+from os.getpass import getuser as user
+from os import environ as env
+
+# TODO DO I PUT THiS HERE? OR RATHER SOMEWHERE ELSE? LIKE SELECTIONS? HAS TO BE AMONG THE FIRST INITS...!
+
+if user == 'manzoni': 
+     env['BASE_DIR'] = '...'
+
+if user == 'cesareborgia': 
+    env['BASE_DIR'] = '...'
+
+cuts = Selections('mmm')
 
 plotter = Plotter (channel        = 'mmm',
                    base_dir        = '/Users/cesareborgia/cernbox/ntuples/2018/',
                    post_fix        = 'HNLTreeProducer_mmm/tree.root',
+                   # baseline_selection = cuts.selections['baseline_low_pT'],
+                   # specific selection = cuts.selections['resonance_vetoes'],
                    lumi           = 59700.,
                    model          = 'NN/mmm/12Nov19_v0/net_model_weighted.h5', 
                    transformation = 'NN/mmm/12Nov19_v0/input_tranformation_weighted.pck',
